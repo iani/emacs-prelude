da9a122600fb3e13b0a4564450a60260b0aa8209
Ioannis Zannos <zannos@gmail.com>
2014-03-25 00:26:04 +0200

    sclang: eval elisp expression from SuperCollider

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(background-mode dark)
  '(cursor-color "#708183")
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" default)))
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" default)))
  '(foreground-color "#708183")
  '(guru-global-mode nil)
  '(hl-sexp-background-colors (quote ("gray0" "#0f003f")))
@@ -17,4 +17,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-sexp-face ((t (:background "#111")))))
